a8832cd518e27d15d7c83df6b6f3bda95c6a9add
Beth-A <elizabeth78@me.com>
2020-04-30 10:07:33 -0400

    Create blank html & js files. Install Express, body-parser, and Request.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+// app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html")
+});
